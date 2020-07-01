79a484b8473356d4241efded0ddf145204fc7c53
Michael Barbour <barbour.michael.0@gmail.com>
2020-06-30 21:25:08 -0400

    Add --2034 option to plot ANSI/CTA-2034-A curves.

--- loudspeaker_directivity.py
+++ loudspeaker_directivity.py
@@ -10,21 +10,27 @@ def usage():
 	print('Usage: {0:s} [OPTION...]'.format(sys.argv[0]))
 	print('')
 	print('Options:')
-	print('    --interval=n         measurement interval in degrees (default: {0:g})'.format(default_interval))
-	print('    --mirror-horizontal  use only positive angles for horizontal orbit')
-	print('    --mirror-vertical    use only positive angles for vertical orbit')
-	print('    -h, --help           show this help text')
+	print('    --interval=n          measurement interval in degrees (default: {0:g})'.format(default_interval))
+	print('    --mirror-horizontal   use only positive angles for horizontal orbit')
+	print('    --mirror-vertical     use only positive angles for vertical orbit')
+	print('    --2034                compute ANSI/CTA-2034-A directivity response')
+	print('    --use-listening-axis  use listening axis for DI calculations')
+	print('    -h, --help            show this help text')
 
 interval = default_interval
 mirror_horiz = False
 mirror_vert = False
+cta_2034 = False
+use_listening_axis = False
 
 try:
 	optlist, args = getopt.gnu_getopt(sys.argv[1:],
-		"h",
+		'h',
 		['interval=',
 		 'mirror-horizontal',
 		 'mirror-vertical',
+		 '2034',
+		 'use-listening-axis',
 		 'help']
 	)
 except getopt.GetoptError as e:
@@ -44,8 +50,16 @@ for o, a in optlist:
 		mirror_horiz = True
 	elif o == '--mirror-vertical':
 		mirror_vert = True
+	elif o == '--2034':
+		cta_2034 = True
+	elif o == '--use-listening-axis':
+		use_listening_axis = True
 	else:
 		assert False, 'unhandled option'
+
+if cta_2034 and 10. % interval != 0:
+	print('error: interval must go into 10 evenly for ANSI/CTA 2034 calculation.')
+	sys.exit(2)
 
 print('Parameters:')
 print('  Measurement interval:   {0:g}°'.format(interval))
@@ -138,6 +152,34 @@ except OSError:
 	print('Failed to open listening_axis.txt; using the axial response')
 	listening_axis = orbit_horiz_positive[0]
 
+def select_list(x, l):
+	r = np.zeros_like(x, dtype=bool)
+	for v in l:
+		r = r | (x == v)
+	return r
+
+def power_average(x):
+	return 10. * np.log10(np.mean(np.power(10., x / 10.), axis=0))
+
+if cta_2034:
+	listening_window = power_average(np.concatenate((
+		orbit_horiz_positive[0:1],
+		orbit_horiz_positive[select_list(weights[:,0],    np.deg2rad([10, 20, 30]))],
+		orbit_horiz_negative[select_list(weights[1:-1,0], np.deg2rad([10, 20, 30]))],
+		orbit_vert_positive[select_list(weights[1:-1,0],  np.deg2rad([10]))],
+		orbit_vert_negative[select_list(weights[1:-1,0],  np.deg2rad([10]))],
+	)))
+	early_reflections = power_average(np.concatenate((
+		orbit_vert_negative[select_list(weights[1:-1,0],  np.deg2rad([20, 30, 40]))],          # Floor
+		orbit_vert_positive[select_list(weights[1:-1,0],  np.deg2rad([40, 50, 60]))],          # Ceiling
+		orbit_horiz_positive[select_list(weights[:,0],    np.deg2rad([0, 10, 20, 30]))],       # Front+
+		orbit_horiz_negative[select_list(weights[1:-1,0], np.deg2rad([10, 10, 30]))],          # Front-
+		orbit_horiz_positive[select_list(weights[:,0],    np.deg2rad([40, 50, 60, 70, 80]))],  # Side+
+		orbit_horiz_negative[select_list(weights[1:-1,0], np.deg2rad([40, 50, 60, 70, 80]))],  # Side-
+		orbit_horiz_positive[select_list(weights[:,0],    np.deg2rad([180, 90]))],             # Rear+
+		orbit_horiz_negative[select_list(weights[1:-1,0], np.deg2rad([90]))],                  # Rear-
+	)))
+
 ##
 ## Plotting
 ##
@@ -147,14 +189,15 @@ import matplotlib.ticker as tkr
 import matplotlib as mpl
 from cycler import cycler
 
-color_black = 'k'
-color_red   = '#ac3536'
-mpl.rcParams['axes.prop_cycle'] = cycler(color=['#ac3536', '#165c79', '#df8b16', '#333', '#76903e', '#aa5799', '#429b8c', '#888'])
-#plt.style.use('tableau-colorblind10')
+colors = ['#ac3536', '#165c79', '#df8b16', '#333', '#76903e', '#aa5799', '#429b8c', '#888']
+mpl.rcParams['axes.prop_cycle'] = cycler(color=colors)
 
-listening_axis_level = np.median(listening_axis[np.logical_and(freqs >= 300, freqs <= 8000)])
-print("listening axis level: {0:g}".format(listening_axis_level))
-y_top = int(round(listening_axis_level/5.))*5+20
+if cta_2034 and not use_listening_axis:
+	norm_level = np.median(listening_window[np.logical_and(freqs >= 300, freqs <= 8000)])
+else:
+	norm_level = np.median(listening_axis[np.logical_and(freqs >= 300, freqs <= 8000)])
+print("norm level: {0:g}".format(norm_level))
+y_top = int(round(norm_level/5.))*5+20
 y_bot = y_top-50
 legend_cols = min(8, int(np.ceil(len(weights) / 5.)))
 
@@ -169,7 +212,7 @@ def setup_line_plot():
 
 	ax.set_xlim(20., 20000.)
 	ax.set_ylim(y_bot, y_top)
-	ax.grid(linestyle=':', which='both', color=color_black, alpha=0.2)
+	ax.grid(linestyle=':', which='both', color='k', alpha=0.2)
 	ax.get_yaxis().set_major_locator(tkr.MultipleLocator(base=10))
 	ax.get_yaxis().set_minor_locator(tkr.MultipleLocator(2))
 	ax.tick_params(which='both', direction='in')
@@ -180,10 +223,21 @@ def setup_line_plot():
 
 fig, ax = setup_line_plot()
 
-ax.plot(freqs, listening_axis, color=color_black, label='Listening Axis')
-ax.plot(freqs, 10. * np.log10(power_spectrum), color=color_red, label='Total Sound Power')
-ax.plot(freqs, 10. * np.log10(np.power(10., listening_axis / 10.) / power_spectrum) + y_bot, color=color_red, linestyle='--', label='Directivity Index (offset {0:d}dB)'.format(y_bot))
-#plt.hlines(listening_axis_level, 20., 20000., colors='grey', linestyles=':', label='Calculated Listening Axis Level')
+if cta_2034:
+	ax.plot(freqs, listening_window, color=colors[4], linestyle='--', label='Listening Window')
+ax.plot(freqs, listening_axis, color='k', label='Listening Axis')
+if cta_2034:
+	ax.plot(freqs, early_reflections, color=colors[1], label='Early Reflections')
+ax.plot(freqs, 10. * np.log10(power_spectrum), color=colors[0], label='Total Sound Power')
+if cta_2034:
+	ax.plot(freqs, (listening_axis if use_listening_axis else listening_window) - early_reflections + y_bot,
+		color=colors[1], linestyle='--', label='Early Reflections DI (offset {0:d}dB)'.format(y_bot))
+ax.plot(freqs, (listening_window if cta_2034 and not use_listening_axis else listening_axis) - (10. * np.log10(power_spectrum)) + y_bot,
+	color=colors[0], linestyle='--', label='{0:s} (offset {1:d}dB)'.format('Sound Power DI' if cta_2034 else 'Directivity Index', y_bot))
+#plt.hlines(norm_level, 20., 20000., colors='grey', linestyles=':', label='Calculated {0:s} Level'.format('Listening Window' if cta_2034 else 'Listening Axis'))
+
+if cta_2034:
+	ax.set_title('ANSI/CTA-2034-A Directivity Response (DI Ref: {0:s})'.format('Listening Axis' if use_listening_axis else 'Listening Window'))
 ax.legend(frameon=False)
 
 print('Writing directivity.png...')
@@ -260,7 +314,7 @@ def do_contour_plot(x, y, z, title, filename):
 	cs2 = ax.contour(
 		cs,
 		levels=cs.levels[::2],
-		colors=color_black,
+		colors='k',
 		linewidths=0.7
 	)
 
@@ -275,7 +329,7 @@ def do_contour_plot(x, y, z, title, filename):
 
 	ax.set_xlim(100., 20000.)
 	ax.set_ylim(-180., 180.)
-	ax.grid(linestyle=':', which='both', color=color_black, alpha=0.2)
+	ax.grid(linestyle=':', which='both', color='k', alpha=0.2)
 	ax.get_yaxis().set_major_locator(tkr.MultipleLocator(base=30))
 	ax.get_yaxis().set_minor_locator(tkr.MultipleLocator(10))
 
@@ -292,7 +346,7 @@ do_contour_plot(
 		orbit_horiz_positive[-1:],
 		np.flip(orbit_horiz_negative, 0),
 		orbit_horiz_positive
-	)) - listening_axis_level,
+	)) - norm_level,
 	'Horizontal Directivity',
 	'directivity_contour_h.png',
 )
@@ -309,7 +363,7 @@ do_contour_plot(
 		orbit_horiz_positive[0:1],
 		orbit_vert_positive,
 		orbit_horiz_positive[-1:],
-	)) - listening_axis_level,
+	)) - norm_level,
 	'Vertical Directivity',
 	'directivity_contour_v.png',
 )
